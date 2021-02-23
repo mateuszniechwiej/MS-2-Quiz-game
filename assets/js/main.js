2fd458d498e78e4646449d61c74505d99b44f04a
mateuszniechwiej <niechwiej.mateusz@gmail.com>
2021-02-23 19:42:05 +0000

    change code for info modal

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,26 @@
 // To activate Info modal using javascript vanila
 function openModal() {
-  document.getElementById("qz-modal--bg").style.display = "block";//display bg as block
-  document.getElementById("qz-Modal").style.display = "block";//display modal as a block
-  document.getElementById("qz-Modal").classList.add("show");//add class to show the modal
+  document.querySelector("#qz-modal--bg").style.display = "block";//display bg as block
+  document.querySelector("#qz-Modal").style.display = "block";//display modal as a block
+  document.querySelector("#qz-Modal").classList.add("show");//add class to show the modal
 }
+const modalOn = document.querySelector("#openModal");
+modalOn.addEventListener('click', openModal);
+
 function closeModal() {
-  document.getElementById("qz-modal--bg").style.display = "none";
-  document.getElementById("qz-Modal").style.display = "none";
-  document.getElementById("qz-Modal").classList.remove("show");// to close the modal remove show class
+  document.querySelector("#qz-modal--bg").style.display = "none";
+  document.querySelector("#qz-Modal").style.display = "none";
+  document.querySelector("#qz-Modal").classList.remove("show");// to close the modal remove show class
 }
+
+const modalOff = document.querySelectorAll(".close");
+
+Array.from(modalOff).forEach( (e) =>{
+  e.addEventListener('click', closeModal);
+});
+  
 // Get the modal
-var infoModal = document.getElementById('qz-Modal');
+var infoModal = document.querySelector('#qz-Modal');
 
 // Click outside the function close the modal
 window.onclick = (e) => {
