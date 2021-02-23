89429a374727067e2cf0c6638c4659a701cbc2bd
mateuszniechwiej <niechwiej.mateusz@gmail.com>
2021-02-23 11:07:43 +0000

    add info modal

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,20 @@
-console.log("test");
+// To activate Info modal using javascript vanila
+function openModal() {
+  document.getElementById("qz-modal--bg").style.display = "block";//display bg as block
+  document.getElementById("qz-Modal").style.display = "block";//display modal as a block
+  document.getElementById("qz-Modal").classList.add("show");//add class to show the modal
+}
+function closeModal() {
+  document.getElementById("qz-modal--bg").style.display = "none";
+  document.getElementById("qz-Modal").style.display = "none";
+  document.getElementById("qz-Modal").classList.remove("show");// to close the modal remove show class
+}
+// Get the modal
+var infoModal = document.getElementById('qz-Modal');
+
+// Click outside the function close the modal
+window.onclick = (e) => {
+  if (e.target == infoModal) {
+    closeModal()
+  }
+};
