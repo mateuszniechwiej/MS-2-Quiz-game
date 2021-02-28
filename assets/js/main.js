78ac2d88f574375519d6ee6974206fd8e031f706
mateuszniechwiej <niechwiej.mateusz@gmail.com>
2021-02-28 16:43:23 +0000

    select&pass difficulty and categories to url

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,8 @@
 // Function to check if user in local Storage(if not open username modal)
 let user = localStorage.getItem("user");
 let highScore = 0;
-
 checkUserData = () => {
-  if ( (user === null) || (user === "")) {
-    //console.log('there is no player')
+  if (user === null || user === "") {
     localStorage.setItem(highScore, 0);
     userModalOn();
   } else {
@@ -12,49 +10,48 @@ checkUserData = () => {
     highScore = localStorage.getItem("highScore");
     if (highScore === null) {
       highScore = 0;
-    };
+    }
     displayUsernameInfo();
     return;
-    
   }
-}
-checkUserData;
+};
+
 // function to store username in localstorage
 userNameSubmit = () => {
   user = document.querySelector("#user").value;
   localStorage.setItem("user", user);
   console.log(user);
-  userModalOff();// close username modal
-  displayUsernameInfo();// function to display user data on dashboard
-}
+  userModalOff(); // close username modal
+  displayUsernameInfo(); // function to display user data on dashboard
+};
 
 displayUsernameInfo = () => {
   document.querySelector(".userName").innerHTML = `${user}`;
   document.querySelector("#highScore").innerHTML = `${highScore} points`;
-}
+};
 
 let subitName = document.querySelector("#submitName");
-subitName.addEventListener('click', userNameSubmit);
-
+subitName.addEventListener("click", userNameSubmit);
+checkUserData();
 
 //Username Modal
 userModalOn = () => {
-  document.querySelector("#usernameModal--bg").style.display = "block"; 
-  document.querySelector("#playerModal").style.display = "block"; 
-  document.querySelector("#playerModal").classList.add("show"); 
-}
+  document.querySelector("#usernameModal--bg").style.display = "block";
+  document.querySelector("#playerModal").style.display = "block";
+  document.querySelector("#playerModal").classList.add("show");
+};
 userModalOff = () => {
   document.querySelector("#usernameModal--bg").style.display = "none";
   document.querySelector("#playerModal").style.display = "none";
-  document.querySelector("#playerModal").classList.remove("show"); 
-}
+  document.querySelector("#playerModal").classList.remove("show");
+};
 // const userOn = document.querySelector("#openUser");
 // userOn.addEventListener("click", userModalOn);
 
-const userOff = document.querySelectorAll(".closeUser")
+const userOff = document.querySelectorAll(".closeUser");
 
 Array.from(userOff).forEach((e) => {
-  e.addEventListener('click', userModalOff);
+  e.addEventListener("click", userModalOff);
 });
 
 // const userModal = document.querySelector("#playerModal");
@@ -64,7 +61,7 @@ openModal = () => {
   document.querySelector("#qz-modal--bg").style.display = "block"; //display bg as block
   document.querySelector("#qz-Modal").style.display = "block"; //display modal as a block
   document.querySelector("#qz-Modal").classList.add("show"); //add class to show the modal
-}
+};
 const infoOn = document.querySelector("#openModal");
 infoOn.addEventListener("click", openModal);
 
@@ -72,7 +69,7 @@ closeModal = () => {
   document.querySelector("#qz-modal--bg").style.display = "none";
   document.querySelector("#qz-Modal").style.display = "none";
   document.querySelector("#qz-Modal").classList.remove("show"); // to close the modal remove show class
-}
+};
 
 const infoOff = document.querySelectorAll(".close");
 
@@ -95,62 +92,62 @@ const iconOn = document.querySelector("#off");
 
 switcher = () => {
   iconOn.classList.toggle("fa-volume-up");
-}
+};
 
 iconOn.addEventListener("click", switcher);
 // Click Sound when volume on
 const click = document.querySelector("#btnClick");
 const allAudio = document.querySelectorAll(".btn");
 
-clickBtn =  () => {
+clickBtn = () => {
   if (document.querySelector(".fa-volume-up")) {
     click.play();
   }
-}
+};
 Array.from(allAudio).forEach((a) => {
   a.addEventListener("click", clickBtn);
 });
 
-
 // questions and game quiz
 const question = document.querySelector("#question");
-const choices = Array.from(document.querySelectorAll('.answer-choice'));
+const choices = Array.from(document.querySelectorAll(".answer-choice"));
 
 let displayedQuestion = {};
 let acceptAnswer = true;
 let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
-//selecting categories
-const categories = Array.from(document.querySelectorAll(".category"));
 
-// geting value of clicked cattegory button
-categories.forEach(category => {
-  category.addEventListener('click', e => {
-    const categoryId = e.target.value;
-    console.log(categoryId);
-  });
-});
-
-// selecting and getting diffculty level for game
+// selecting and getting diffculty level and categories
 
 const difficulty = Array.from(document.querySelectorAll(".difficulty"));
+const categories = Array.from(document.querySelectorAll(".category"));
 
-difficulty.forEach(level => {
-  level.addEventListener('click', e => {
-    const difficultyLvl = e.target.value;
-    console.log(difficultyLvl);
+difficulty.forEach((level) => {
+  level.addEventListener("click", (e) => {
+    if ((difficultyLvl = e.target.value)) 
+      return;
+    
   });
 });
 
-// const url = `https://opentdb.com/api.php?amount=10&category=${categoryId}&difficulty=${difficultyLvl}`
+categories.forEach((category) => {
+  category.addEventListener("click", (e) => {
+    if ((categoryId = e.target.value)) 
+      return;
+  });
+});
+settings = () => {
+  const url = `https://opentdb.com/api.php?amount=10&category=${categoryId}&difficulty=${difficultyLvl}`;
+  console.log(url);
+};
+const start = document.querySelector("#start");
 
-
-
+start.addEventListener('click', settings);
 
 
 // CONSTANST VARIABLES
-// Bonus point will depend on the level of quiz difficulty the player will chose 
+// Bonus point will depend on the level of quiz difficulty the player will chose
 const EASY_BONUS = 10;
 const MEDIUM_BONUS = 12;
 const HARD_BONUS = 15;
