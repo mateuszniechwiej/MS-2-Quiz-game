0dffcb1d9022978626b824ef2b255c5fc5b8035e
mateuszniechwiej <niechwiej.mateusz@gmail.com>
2021-03-03 14:36:56 +0000

    add user.js for localstorage data

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,39 +1,3 @@
-// Function to check if user in local Storage(if not open username modal)
-let user = localStorage.getItem("user");
-let highScore = 0;
-checkUserData = () => {
-  if (user === null || user === "") {
-    localStorage.setItem(highScore, 0);
-    userModalOn();
-  } else {
-    user = localStorage.getItem("user");
-    highScore = localStorage.getItem("highScore");
-    if (highScore === null) {
-      highScore = 0;
-    }
-    displayUsernameInfo();
-    return;
-  }
-};
-
-// function to store username in localstorage
-userNameSubmit = () => {
-  user = document.querySelector("#user").value;
-  localStorage.setItem("user", user);
-  console.log(user);
-  userModalOff(); // close username modal
-  displayUsernameInfo(); // function to display user data on dashboard
-};
-
-displayUsernameInfo = () => {
-  document.querySelector(".userName").innerHTML = `${user}`;
-  document.querySelector("#highScore").innerHTML = `${highScore} points`;
-};
-
-let subitName = document.querySelector("#submitName");
-subitName.addEventListener("click", userNameSubmit);
-checkUserData();
-
 //Username Modal
 userModalOn = () => {
   document.querySelector("#usernameModal--bg").style.display = "block";
@@ -109,7 +73,7 @@ window.onclick = (e) => {
   }
 };
 
-
+// -------------------------------------Game Settings-------------------------------------------------
 // toggle sound icon on
 const iconOn = document.querySelector("#off");
 
@@ -130,8 +94,6 @@ clickBtn = () => {
 Array.from(allAudio).forEach((a) => {
   a.addEventListener("click", clickBtn);
 });
-
-
 
 // selecting and getting diffculty level and categories
 
---
+++ assets/js/user.js
@@ -0,0 +1,35 @@
+// Function to check if user in local Storage(if not open username modal)
+let user = localStorage.getItem("user");
+let highScore = 0;
+checkUserData = () => {
+    if (user === null || user === "") {
+        localStorage.setItem(highScore, 0);
+        userModalOn();
+    } else {
+        user = localStorage.getItem("user");
+        highScore = localStorage.getItem("highScore");
+        if (highScore === null) {
+            highScore = 0;
+        }
+        displayUsernameInfo();
+        return;
+    }
+};
+
+// function to store username in localstorage
+userNameSubmit = () => {
+    user = document.querySelector("#user").value;
+    localStorage.setItem("user", user);
+    console.log(user);
+    userModalOff(); // close username modal
+    displayUsernameInfo(); // function to display user data on dashboard
+};
+
+displayUsernameInfo = () => {
+    document.querySelector(".userName").innerHTML = `${user}`;
+    document.querySelector("#highScore").innerHTML = `${highScore} points`;
+};
+
+let subitName = document.querySelector("#submitName");
+subitName.addEventListener("click", userNameSubmit);
+checkUserData();
